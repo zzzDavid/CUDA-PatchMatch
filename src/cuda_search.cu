88c1f2d634988e2290c9691d27ca0f6509d18f48
zzzDavid <33577135+zzzDavid@users.noreply.github.com>
2019-06-17 20:33:33 +0800

    basic framework done

--- src/cuda_search.cu
+++ src/cuda_search.cu
@@ -1,22 +1,53 @@
 #include "cuda_search.h"
 
 
-__global__ void sayHelloWorld() {
-	printf("HelloWorld! GPU \n");
-	//cout << "HelloWorld! GPU" << endl;     //不能使用cout, std命名不能使用到GPU上
+__global__ void integer_patch_match(unsigned char* in0_gpu, int width, int height, int blockw, int blockh, int regionw, int regionh, float** motion_x, float** motion_y) 
+{
+	int x_index = threadIdx.x;
+	int y_index = threadIdx.y;
+
+	// get block head pointer
+	unsigned char* block_head = in0_gpu + 2 * y_index * width + 2 * x_index;
+
+}
+
+__global__ void interpolate_patch_match()
+{
+
 }
 
 
-int cuda_full_search(unsigned char *out, unsigned char *in0, unsigned char *in1, int width, int height) {
+int cuda_full_search(unsigned char *out_gpu, unsigned char *in0_gpu, unsigned char *in1_gpu, int width, int height) {
 
-	// do something here
-	printf("HelloWorld! CPU \n");
+	/* Initialize Parameters */
+	int blockw = 16;				// search block width 
+	int blockh = 16;				// search block height
+	int bnumx = width / blockw;		// block number in x direction
+	int bnumy = height / blockh;	// block number in y direction
+	int regionw = 16 * 3;			// block search region width
+	int regionh = 16 * 3;			// block search region heigt
 
-	// call kernel function
-	sayHelloWorld << <1, 10 >> > ();   //调用GPU上执行的函数，调用10个GPU线程
+	int blockwf = 2;				// search block width of interpolation search
+	int blockhf = 2;				// search block height of interpolation search
+	int interprate = 2;				// interpolate rate
+	int regionwf = 16;				// search region width of interpolation search
+	int regionhf = 16;				// search region height of interpolation search
 
-	// explicitly free resources of current thread
+	/* Motion Vectors Initialization */
+	float **motion_x = new float*[bnumy];				// x motion vector for each block,  2D float array
+	for (int i = 0; i < bnumy; i++)
+		motion_x[i] = new float[bnumx];
+	float **motion_y = new float*[bnumy];				// y motion vector for each block,  2D float array
+	for (int i = 0; i < bnumy; i++)
+		motion_y[i] = new float[bnumx];					
+
+	/* Parallel Integer Patch Match */
+	dim3 threadsPerBlock(bnumy, bnumx);
+	integer_patch_match <<<1, threadsPerBlock>>> (in0_gpu, width, height, blockw, blockh, regionw, regionh, motion_x, motion_y);
+
+	interpolate_patch_match <<<1, 1>>>();
+
+	// explicitly free resources
 	cudaDeviceReset();
-
 }
 
